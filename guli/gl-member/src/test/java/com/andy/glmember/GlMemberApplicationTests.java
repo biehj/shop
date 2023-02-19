655a8da2c00331ad033b68e38f57d2e71d2f6d9b
lk <1250441769@qq.com>
2023-02-19 10:39:58 +0800

    初始化

---
+++ guli/gl-member/src/test/java/com/andy/glmember/GlMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.glmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ guli/gl-order/src/main/java/com/andy/glorder/GlOrderApplication.java
@@ -0,0 +1,13 @@
+package com.andy.glorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlOrderApplication.class, args);
+    }
+
+}
